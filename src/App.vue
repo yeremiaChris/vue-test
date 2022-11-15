c09b5cfd127049dc4dc41dbb38af97d5c9ab318d
yeremiaChris <yeremia997@gmail.com>
2022-11-15 20:06:32 +0700

    create vue-app, create navbar-menu comP

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavbarMenuVue />
+</template>
+
+<script setup>
+import NavbarMenuVue from "./components/Navbar-menu.vue";
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      header: " test",
+    };
+  },
+};
+</script>
---
+++ src/components/Navbar-menu.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="py-6 shadow">
+    <div class="container box-between mx-auto">
+      <!-- logo -->
+      <img src="/images/logo.png" alt="logo" />
+
+      <!-- list of menus -->
+      <ul class="box-between gap-6">
+        <li
+          v-for="(menu, index) in menus"
+          :key="index + menu.title"
+          class="px-4 py-2 rounded-full box-center bg-blue-400 text-white font-medium"
+        >
+          <a href="#">
+            {{ menu.title }}
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        {
+          title: "Company",
+        },
+        {
+          title: "Services",
+        },
+        {
+          title: "Our Core Values",
+        },
+        {
+          title: "Our Speciality",
+        },
+        {
+          title: "Footer",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
